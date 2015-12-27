696c8583ed2acd0770354ce459104841201b9263
Matt Wemmie <matt.wemmie@gmail.com>
2015-12-26 22:02:52 -0600

    added scale on mousewheel scroll functionality. #10

--- app/scripts/classdiagrams.js
+++ app/scripts/classdiagrams.js
@@ -496,6 +496,35 @@ paper.on('cell:contextmenu ',
     }
 );
 
+// on mouse wheel zoom
+// http://jsfiddle.net/kumilingus/atnoopkm/
+function onMouseWheel(e) {
+
+    e.preventDefault();
+    e = e.originalEvent;
+
+    var delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail))) / 50;
+    var offsetX = (e.offsetX || e.clientX - $(this).offset().left); // offsetX is not defined in FF
+    var offsetY = (e.offsetY || e.clientY - $(this).offset().top); // offsetY is not defined in FF
+    var p = offsetToLocalPoint(offsetX, offsetY);
+    var newScale = V(paper.viewport).scale().sx + delta; // the current paper scale changed by delta
+
+    if (newScale > 0.4 && newScale < 2) {
+        paper.setOrigin(0, 0); // reset the previous viewport translation
+        paper.scale(newScale, newScale, p.x, p.y);
+    }
+}
+
+function offsetToLocalPoint(x, y) {
+    var svgPoint = paper.svg.createSVGPoint();
+    svgPoint.x = x;
+    svgPoint.y = y;
+    // Transform point into the viewport coordinate system.
+    var pointTransformed = svgPoint.matrixTransform(paper.viewport.getCTM().inverse());
+    return pointTransformed;
+}
+
+paper.$el.on('mousewheel DOMMouseScroll', onMouseWheel);
 
 
 
